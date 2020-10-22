7039e6eee5a0caa606801d2fb7599dac956b4b25
Rome Bell <romebell5@gmail.com>
2020-10-22 07:15:18 -0700

    [check-number] Write checkNumber function 5 times

---
+++ check-number.js
@@ -0,0 +1,72 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(2));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(4));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(7));
+
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positve';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(9));
+
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else  {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(1234567));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else  {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(50000));
