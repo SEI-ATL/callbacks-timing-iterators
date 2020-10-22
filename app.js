e8f2492b42767de058a5f33e060e3518bf819f94
Rome Bell <romebell5@gmail.com>
2020-10-22 10:34:10 -0700

    [app] Add snippets from lecture

---
+++ app.js
@@ -0,0 +1,47 @@
+function alarm() {
+	console.log("Wake up!");
+}
+setTimeout(alarm, 10000);
+
+function annoy() {
+  console.log('Are we there yet?');
+}
+setInterval(annoy, 1000);
+
+function alarm() {
+	console.log("Wake up!");
+}
+let firstAlarm = setTimeout(alarm, 3000);
+let secondAlarm = setTimeout(alarm, 6000);
+
+
+function annoy() {
+	console.log('Are we there yet?');
+}
+function shutDown() {
+	console.log('No!');
+}
+let kids = setInterval(annoy, 1000);
+let parents = setInterval(shutDown, 3200);
+
+
+function annoy() {
+	console.log('Are we there yet?');
+}
+function hush() {
+	clearInterval(moreKids);
+}
+let moreKids = setInterval(annoy, 1000);
+setTimeout(hush, 10000);
+
+function alarmRing() {
+	console.log('RRRIIINNNGGGGG');
+}
+
+function turnOffSnooze() {
+	console.log("Turning snooze off now");
+	clearTimeout(snoozeAlarm);
+}
+
+let snoozeAlarm = setTimeout(alarmRing, 3000);
+let snoozeOff = setTimeout(turnOffSnooze, 5000);
