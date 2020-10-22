03842b488b3ff6c12bcb17d7ad99824432307fa8
Rome Bell <romebell5@gmail.com>
2020-10-22 10:32:20 -0700

    [hello] Add sayHello function

---
+++ hello.js
@@ -0,0 +1,35 @@
+function sayHello(name) {
+    let msg = `Hello ${name}`;
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = `Hello ${name}`;
+    return msg;
+}
+
+console.log(sayHello('Rome'));
+
+function sayHello(name) {
+    let msg = `Hello ${name}`;
+    return msg;
+}
+
+console.log(sayHello('Pete'));
+
+function sayHello(name) {
+    let msg = `Hello ${name}`;
+    return msg;
+}
+
+console.log(sayHello('Dexter'));
+
+function sayHello(name) {
+    let msg = `Hello ${name}`;
+    return msg;
+}
+
+console.log(sayHello('Mike'));
+
